1820508a22d7fd73be4b8965e26a57810310cb36
kof821117 <jackysfbay@gmail.com>
2016-02-06 02:11:21 -0800

    update

---
+++ JavaScript/List_Filtering.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Jacky on 2/5/16.
+ */
+
+/*
+ In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+ Example
+
+ filter_list([1,2,'a','b']) == [1,2]
+ filter_list([1,'a','b',0,15]) == [1,0,15]
+ filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+ */
+
+
+window.onload = function () {
+
+    function filter_list(arr) {
+        // Return a new array with the strings filtered out
+        var list = [];
+        var pattern = /^[0-9]+$/;
+        for (var i = 0; i < arr.length; i++) {
+            if ((pattern.test(arr[i]) ==true) && (typeof arr[i] == "number")) {
+                list.push(arr[i]);
+            }
+        }
+        return list;
+    }
+
+    alert(filter_list([1,2,'aasf','1','123',123]))
+
+}
