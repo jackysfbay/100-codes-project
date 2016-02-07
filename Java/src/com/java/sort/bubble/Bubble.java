5bcc54fe987b8b78fc7bcd92850a891fd41bc7dd
kof821117 <jackysfbay@gmail.com>
2016-02-07 13:06:34 -0800

    update

---
+++ Java/src/com/java/sort/bubble/Bubble.java
@@ -0,0 +1,22 @@
+package com.java.sort.bubble;
+
+import java.util.Random;
+
+public class Bubble {
+	public static void main(String[] args) {
+		int[] arr = { 1, 4, 333, 45, 6, 7 };
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (Integer list : arr) {
+			System.out.print(list + " ");
+		}
+	}
+}
