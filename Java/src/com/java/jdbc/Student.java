e75e778606d34b38304d50beddfeede8002ca728
kof821117 <jackysfbay@gmail.com>
2016-02-08 11:56:35 -0800

    update

---
+++ Java/src/com/java/jdbc/Student.java
@@ -0,0 +1,44 @@
+package com.java.jdbc;
+
+public class Student {
+	private String id;
+	private String name;
+	private String sex;
+	
+	public Student() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	private String age;
+}
