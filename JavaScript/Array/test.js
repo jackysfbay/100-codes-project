0690a10a9ca81e64cf3a6cf7afab74e3dbbfde67
kof821117 <jackysfbay@gmail.com>
2016-02-09 18:22:49 -0800

    update

---
+++ JavaScript/Array/test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jacky on 2/9/16.
+ */
+
+/*
+
+ */
+
+
+
+
+
+window.onload= function () {
+
+    alert('asd')
+}
--- JavaScript/Remove_the_minimum.js
+++ JavaScript/Remove_the_minimum.js
@@ -3,7 +3,17 @@
  */
 
 /*
+ Task
 
+ Given an array of integers, remove the smallest value. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+ Don't change the order of the elements that are left.
+
+ Examples
+
+ removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+ removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+ removeSmallest([2,2,1,2,1]) = [2,2,2,1]
  */
 
 ///(\d)(?=(\d{3})+$)/g, '$1,'
