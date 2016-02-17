7fecfa54a7e3843ec7baa76f5932d3ad72693afe
kof821117 <jackysfbay@gmail.com>
2016-02-16 21:50:36 -0800

    update

---
+++ SeleniumWebDriver01/01/src/com/selenium/Test.java
@@ -0,0 +1,30 @@
+package com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import bsh.commands.dir;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://localhost:63342/JS_Test01/test.html");
+		
+		WebElement btn = driver.findElement(By.name("btn"));
+		//query.sendKeys("selenium");
+		
+		//WebElement btn = driver.findElement(By.id("su"));
+		Thread.sleep(5000);
+		btn.click();
+		Thread.sleep(5000);
+		
+//		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+}
