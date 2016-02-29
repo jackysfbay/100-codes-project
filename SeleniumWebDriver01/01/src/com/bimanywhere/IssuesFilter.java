22fa330142df7b06a2a33856c715d4737f796104
kof821117 <jackysfbay@gmail.com>
2016-02-29 00:23:01 -0800

    update

---
+++ SeleniumWebDriver01/01/src/com/bimanywhere/IssuesFilter.java
@@ -0,0 +1,45 @@
+package com.bimanywhere;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class IssuesFilter {
+	WebDriver driver;
+
+	@Test
+	public void filterIssue() throws InterruptedException {
+		GetUserSeesion session = new GetUserSeesion();
+		driver = session.getSeesion();
+		Thread.sleep(1000);
+		WebElement projectsTable = driver.findElement(By.className("table"));
+		List<WebElement> projectRows = projectsTable.findElements(By.xpath(".//tbody/tr"));
+		(projectRows.get(0)).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.className("form-control")).sendKeys("Issue");
+		Thread.sleep(5000);
+		WebElement issuesTable = driver.findElement(By.className("table"));
+		List<WebElement> isssuesRows = issuesTable.findElements(By.xpath(".//tbody/tr"));
+		String rowSize = isssuesRows.size() + "";
+		String filterNo = driver.findElement(By.className("pagedisplay")).getText();
+		String str1 = filterNo.replaceAll("[^0-9]*([0-9]*)[^0-9]*", "$1\'");
+		String[] str2 = str1.split("'");
+		Boolean result = rowSize.equals(str2[2]);
+		assertEquals(true, result);
+		Thread.sleep(2000);
+	}
+
+	@After
+	public void exit() {
+		
+		driver.quit();
+	}
+
+}
--- SeleniumWebDriver01/01/src/com/selenium/Test01.java
+++ SeleniumWebDriver01/01/src/com/selenium/Test01.java
@@ -2,13 +2,30 @@ package com.selenium;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test01 {
 	public static void main(String[] args) {
-		   
-		String msg = "The issue Issue - AutoTest-Time-2016-02-28 21:03:01 have been created.";
+//		   
+//		String msg = "The issue Issue - AutoTest-Time-2016-02-28 21:03:01 have been created.";
+//		
+//	    Boolean b = msg.contains("AutoTest-Time-2016-02-28 21:03:01 have been created");
+//		System.out.println(b);
 		
-	    Boolean b = msg.contains("AutoTest-Time-2016-02-28 21:03:01 have been created");
-		System.out.println(b);
+		String str ="Page 22 of 23 (220)";
+		
+	 
+
+		
+		String str1 = str.replaceAll("[^0-9]*([0-9]*)[^0-9]*", "$1\'");
+		String[] str2=str1.split("'");
+
+		System.out.println(str2[2]);
+
+		
+	
+		
+		
 	}
 }
